4222ac42b687c0be0c5ca2f41b52266915fc04b2
xqz0307 <xqz0307@163.com>
2023-01-11 22:19:56 +0800

    first commit

--- src/main/java/com/yanggu/git/GitTest.java
+++ src/main/java/com/yanggu/git/GitTest.java
@@ -2,7 +2,7 @@ package com.yanggu.git;
 
 /**
  * @author bigSea
- * @create 2023-01-11 22:01:33
+ * @createDate 2023-01-11 22:01:33
  */
 public class GitTest {
     public static void main(String[] args) {
