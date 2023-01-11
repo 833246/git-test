030f613ac508f9e4bb4746d509fa4c988ba8a978
xqz0307 <xqz0307@163.com>
2023-01-11 22:08:56 +0800

    first commit

---
+++ src/main/java/com/yanggu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yanggu.git;
+
+/**
+ * @author bigSea
+ * @create 2023-01-11 22:01:33
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
